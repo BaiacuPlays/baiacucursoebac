3edeaa1c92f4b516bb67acb87aef54bb64b8dbe0
BaiacuPlays <andreibonatto8@gmail.com>
2023-12-03 15:06:58 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+const resultadoMultiplicacao = multiplicar(2, 3);
+console.log("Resultado da multiplicação:", resultadoMultiplicacao);
+const mensagemSaudacao = saudacao("João");
+console.log(mensagemSaudacao);
